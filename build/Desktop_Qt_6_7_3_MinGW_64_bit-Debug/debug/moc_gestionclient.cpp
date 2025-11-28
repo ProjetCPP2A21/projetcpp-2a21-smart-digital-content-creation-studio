73f8eeb059929b87b32f1ef00e49103b66c71ec9
selimaoueslati <selima.oueslati@esprit.tn>
2025-11-28 15:14:32 +0100

    quelques retouches métiers

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/debug/moc_gestionclient.cpp
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/debug/moc_gestionclient.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "E:/ProjetC++/Interface_gestionclient_/gestionclient.h"
+#include "../../../gestionclient.h"
 #include <QtCore/qmetatype.h>
 
 #include <QtCore/qtmochelpers.h>
